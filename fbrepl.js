e233b782387fc84062a6c2e3c113beefd82904fc
Josh Dague <daguej@email.uc.edu>
2015-05-29 15:19:05 -0700

    add count, has, compareKeys

--- fbrepl.js
+++ fbrepl.js
@@ -1,5 +1,6 @@
 var repl = require('repl'),
-	util = require('util');
+	util = require('util'),
+	fs = require('fs');
 
 Firebase = require('firebase');
 
@@ -12,13 +13,51 @@ test=root.child('test');
 
 var last = null;
 // A dirty, dirty hack to work around the Firebase client's minified symbols...
-test.__proto__.__proto__.dump = function(depth) {
+var Ref = test.__proto__.__proto__;
+
+Ref.dump = function(depth) {
 	this.once('value', function(snap) {
 		last = snap.val();
 		console.log(util.inspect(last, {
 			colors: true,
 			depth: typeof depth != 'undefined' ? depth : 2
 		}));
+	});
+};
+
+Ref.count = function() {
+	this.once('value', function(snap) {
+		var o = snap.val(), i = 0;
+		for (var k in o) i++;
+		console.log(i);
+	});
+};
+
+Ref.has = function(key) {
+	this.once('value', function(snap) {
+		var o = snap.val(), i = 0;
+		for (var k in o) {
+			if (key in o[k]) {
+				++i;
+				console.log(k);
+			}
+		}
+		console.log('Total =', i);
+	});
+}
+
+Ref.compareKeys = function(json) {
+	this.once('value', function(snap) {
+		var o = snap.val(),
+			arr = JSON.parse(fs.readFileSync(json)),
+			c = {};
+
+		for (var i = 0; i < arr.length; i++) {
+			c[arr[i]] = true;
+		}
+		for (var k in o) {
+			if (!c[k]) console.log(k);
+		}
 	});
 };
 
